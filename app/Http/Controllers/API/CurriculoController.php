4934f938270a456bf0902ac83cb69f529ec6d7be
Alejandro Bermúdez <2060559@alu.murciaeduca.es>
2026-02-05 11:24:46 +0100

    otra forma con Gate

--- app/Http/Controllers/API/CurriculoController.php
+++ app/Http/Controllers/API/CurriculoController.php
@@ -47,7 +47,7 @@ class CurriculoController extends Controller
      */
     public function update(Request $request, Curriculo $curriculo)
     {
-        abort_if (! Gate::allows('update-curriculo', $curriculo), 403);
+        abort_if (! Gate::allows('owner', $curriculo), 403);
 
         $curriculoData = [
             'user_id' => Auth::user()->id
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -9,14 +9,22 @@ use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider{
+
     public function boot(): void {
 
         ResetPassword::createUrlUsing(function (object $notifiable, string $token) {
-                return config('app.frontend_url')."/password-reset/$token?email={$notifiable->getEmailForPasswordReset()}";
-            });
 
-        Gate::define('update-curriculo', function (User $user, Curriculo $curriculo) {
-            return $user->id === $curriculo->user_id;
-            });
-        }
+            return config('app.frontend_url')."/password-reset/$token?email={$notifiable->getEmailForPasswordReset()}";
+        });
+
+
+        Gate::define('owner', function (User $user, $recurso, $ownerField = 'user_id') {
+
+            if (!$recurso || !$recurso->hasAttribute($ownerField)){
+                return false;
+            }
+
+            return $user->id === $recurso->user_id;
+        });
+    }
 }
